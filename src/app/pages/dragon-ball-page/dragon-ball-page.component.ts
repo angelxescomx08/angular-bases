36626c285986cfe10283af52dc6c66878faf835d
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-08-16 14:27:49 -0600

    Add Dragon Ball page component, routing, and navbar link; include styles and tests

---
+++ src/app/pages/dragon-ball-page/dragon-ball-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragon-ball-page',
+  imports: [],
+  templateUrl: './dragon-ball-page.component.html',
+  styleUrl: './dragon-ball-page.component.css'
+})
+export class DragonBallPageComponent {
+
+}
