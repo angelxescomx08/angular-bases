077d2a4e29339923ef55e7c4351d1bf5743b4409
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-08-16 14:48:56 -0600

    Refactor Dragon Ball page to use dynamic character list; update HTML for character display and input fields

--- src/app/pages/dragon-ball-page/dragon-ball-page.component.ts
+++ src/app/pages/dragon-ball-page/dragon-ball-page.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+type Character = {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragon-ball-page',
@@ -7,5 +13,11 @@ import { Component } from '@angular/core';
   styleUrl: './dragon-ball-page.component.css'
 })
 export class DragonBallPageComponent {
-
+  public characters = signal<Character[]>([
+    { id: 1, name: "Goku", power: 100 },
+    { id: 2, name: "Vegeta", power: 90 },
+    { id: 3, name: "Krilin", power: 80 },
+    { id: 4, name: "Piccolo", power: 70 },
+    { id: 5, name: "Gohan", power: 60 },
+  ]);
 }
