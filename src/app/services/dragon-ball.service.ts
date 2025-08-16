b4904229b1adb0f17aa3118780611184a9a98cc2
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-08-16 16:35:52 -0600

    Implementar carga de personajes desde el almacenamiento local y guardar cambios automáticamente; se actualiza el servicio DragonBall para mejorar la gestión de personajes.

--- src/app/services/dragon-ball.service.ts
+++ src/app/services/dragon-ball.service.ts
@@ -1,15 +1,18 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../types/character.type';
+
+function loadFromLocalStorage(): Character[] {
+  const characters = localStorage.getItem("characters");
+  return characters ? JSON.parse(characters) : [];
+}
 
 @Injectable({ providedIn: 'root' })
 export class DragonBallService {
-  public characters = signal<Character[]>([
-    { id: 1, name: "Goku", power: 100 },
-    { id: 2, name: "Vegeta", power: 90 },
-    { id: 3, name: "Krilin", power: 80 },
-    { id: 4, name: "Piccolo", power: 70 },
-    { id: 5, name: "Gohan", power: 60 },
-  ]);
+  public characters = signal<Character[]>(loadFromLocalStorage());
+
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem("characters", JSON.stringify(this.characters()));
+  });
 
   addCharacter(character: Character) {
     this.characters.update(current => [...current, character]);
